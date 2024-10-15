6a1ebe88389afb662910f8866fec2a89ea512276
cambram <91072006+cambram@users.noreply.github.com>
2024-10-15 17:59:17 +0200

    init commit

---
+++ scripts/home.js
@@ -0,0 +1,11 @@
+document.getElementById('btn-start-five-senses').addEventListener('click', async () => {
+    window.location.href = 'mindful-activities/five-senses.html';
+});
+
+document.getElementById('btn-start-mindful-meditation').addEventListener('click', async () => {
+    window.location.href = 'mindful-activities/mindful-meditation.html';
+});
+
+document.getElementById('btn-start-body-scan').addEventListener('click', async () => {
+    window.location.href = 'mindful-activities/body-scan.html';
+});
